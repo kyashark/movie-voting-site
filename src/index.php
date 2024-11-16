51c7780c7e8bd56cc64aceaf33172cf624741c86
shakya <152844822+kyashark@users.noreply.github.com>
2024-11-17 02:06:52 +0530

    Update home page

--- src/index.php
+++ src/index.php
@@ -33,14 +33,32 @@
 <main>
     <div class="container">
       <div class="col-left">
-          <div class="row row1"></div>
+          <div class="row row1">
+            <p class="topic-5">Welcome</p>
+            <h1 class="topic-1"><span class="zen-font">Zen</span>ith Movie Votes</h1>
+            <p class="topic-4">Celebrate and vote for your 
+            favorite movies</p>
+          </div>
           <div class="row row2">
-            <div class="row-col col1"></div>
-            <div class="row-col col2"></div>
+            <div class="row-col col1">
+              <div class="arrow">
+                <img src="../images/arrow.png">
+              </div>
+              <p class=topic-6>
+                <span class="line-1">Top Voted</span> 
+                <span class="line-2">Movies</span>
+              </p>
+            </div>
+            <div class="row-col col2">
+            <div class="arrow">
+                <img src="../images/arrow.png">
+              </div>
+              <p class=topic-6>New Release</p>
+            </div>
           </div>
       </div>
      <div class="col-right">
-
+        <button class="vote-button">Vote Now</button>
      </div>
     </div>
   </main>
