803bc8238dedd6a2742f2096e25188070fe86b5d
shakya <152844822+kyashark@users.noreply.github.com>
2024-12-05 01:05:01 +0530

    Implement movie page frontend

--- app/views/user/movie.php
+++ app/views/user/movie.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= BASE_URL ?>/css/style.css">
 
-
     <title>Movie</title>
 </head>
 
@@ -24,7 +23,6 @@
                 </ul>
             </div>
             <div class="right-nav">
-                <!-- <button class="login-btn">Login</button> -->
                 <span class="username-text">
                     <a href="#">
                         <?php echo $username ?>
@@ -48,42 +46,63 @@
     <main>
         <div class="movie-container">
 
-            <div class="filter-contianer">
+            <div class="filter-container">
                 <div class="filter-tab">
-                    <button class="filter-btn">Top Voted</button>
-                    <button class="filter-btn">New Realease</button>
-                    <button class="filter-btn">Alphabetical</button>
+                    <button class="filter btn">Top Voted</button>
+                    <button class="filter btn">New Realease</button>
+                    <button class="filter btn">Alphabetical</button>
                 </div>
-                <div class="search-bar">
-                    <input type="search" placeholder="Search">
-                </div>
-            </div>
-            <div class="genre-container">
-                <button class="gener-btn">Action</button>
-                <button class="gener-btn">Adventure</button>
-                <button class="gener-btn">Comedy</button>
-                <button class="gener-btn">Drama</button>
-                <button class="gener-btn">Horror</button>
-                <button class="gener-btn">Sci-Fi</button>
-                <button class="gener-btn">Thriller</button>
-                <button class="gener-btn">Romance</button>
-                <button class="gener-btn">Fantasy</button>
-            </div>
-            <div class="movie-grid">
-                <div class="card">Your name</div>
-                <div class="card">Your name</div>
-                <div class="card">Your name</div>
-                <div class="card">Your name</div>
-                <div class="card">Your name</div>
-                <div class="card">Your name</div>
-                <div class="card">Your name</div>
-                <div class="card">Your name</div>
-                <div class="card">Your name</div>
-                <div class="card">Your name</div>
-                <div class="card">Your name</div>
-                <div class="card">Your name</div>
+
+                <select name="filters" id="filters">
+                    <option value="top">Top Voted</option>
+                    <option value="new">New Realease</option>
+                    <option value="alphabetical">Alphabetical</option>
+                </select>
+
+                <input type="search" placeholder="Search" class="search-input">
             </div>
 
+            <div class="genre-container">
+                <button class="gener">Action</button>
+                <button class="gener">Adventure</button>
+                <button class="gener">Comedy</button>
+                <button class="gener">Drama</button>
+                <button class="gener">Horror</button>
+                <button class="gener">Sci-Fi</button>
+                <button class="gener">Thriller</button>
+                <button class="gener">Romance</button>
+                <button class="gener">Fantasy</button>
+            </div>
+
+            <div class="movie-grid">
+
+                <div class="card" style="background-image: url('<?=BASE_URL ?>/images/ponyo.jpeg');" id="card">
+                    <div class="card-label" id="card-label">
+                        <span>Ponyo</span>
+                        <div class="movie-tab">
+                            <span class="year">2018</span>
+                            <div class=vote-tab>
+                                <span class="vote-count">200</span>
+                                <span class="material-symbols-outlined">&#xe87d;</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card" style="background-image: url('<?=BASE_URL ?>/images/ponyo.jpeg');" id="card">
+                    <div class="card-label" id="card-label">
+                        <span>Ponyo</span>
+                        <div class="movie-tab">
+                            <span class="year">2018</span>
+                            <div class=vote-tab>
+                                <span class="vote-count">200</span>
+                                <span class="material-symbols-outlined" id="heart">&#xe87d;</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </main>
     <script src="<?= BASE_URL ?>/js/index.js"></script>
--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,79 @@
 // Hamburger Menu
 const menuItem = document.getElementById("menu-item");
 
-menuItem.addEventListener("click",function(){
+menuItem.addEventListener("click", function () {
     document.getElementById("nav-bar").classList.toggle("show");
 
-    document.querySelectorAll("span").forEach((bar,index) =>{
+    document.querySelectorAll("span").forEach((bar, index) => {
         bar.classList.toggle("change");
     });
 })
+
+
+
+// Movie card label display
+
+const cardItems = document.querySelectorAll(".card");
+const labelItems = document.querySelectorAll(".card-label");
+
+cardItems.forEach((cardItem, index) => {
+    const labelItem = labelItems[index];
+  
+    labelItem.style.display = "none";
+  
+    cardItem.addEventListener("mouseenter", () => {
+      labelItem.style.display = "flex"; 
+    });
+  
+    cardItem.addEventListener("mouseleave", () => {
+      labelItem.style.display = "none";
+    });
+  });
+
+
+
+// Heart click 
+const heartItems = document.querySelectorAll(".material-symbols-outlined");
+
+heartItems.forEach((heartItem) => {
+    heartItem.addEventListener("click", () => {
+      const currentStyle = window.getComputedStyle(heartItem).fontVariationSettings;
+  
+      if (currentStyle.includes('"FILL" 0')) {
+        heartItem.style.fontVariationSettings = '"FILL" 1, "wght" 400, "GRAD" 0, "opsz" 0';
+      } else {
+        heartItem.style.fontVariationSettings = '"FILL" 0, "wght" 400, "GRAD" 0, "opsz" 0';
+      }
+    });
+  });
+
+
+
+// Filter buttons behavior 
+const filterButtons = document.querySelectorAll(".filter");
+
+filterButtons.forEach(button =>{
+    button.addEventListener("click",()=>{
+        filterButtons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+    });
+});
+
+
+// Genre button behavior
+const generButtons = document.querySelectorAll(".gener");
+
+
+generButtons.forEach(button =>{
+    button.addEventListener("click", () => {
+
+       if(button.classList.contains("active")){
+        button.classList.remove("active");
+       }else{
+        button.classList.add("active");
+       }
+    });    
+});
+
+
+
